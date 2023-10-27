5d69a86c9d12bc00c021314763064f0131088698
Wilson E. Alvarez <wilson.e.alvarez@rubonnek.com>
2023-10-26 20:55:40 -0400

    Remove packages I no longer maintain

--- cocos2d-x-src/cocos2d-x.sh
+++
@@ -1,6 +0,0 @@
-export COCOS_X_ROOT=/opt/cocos2d-x
-export COCOS_CONSOLE_ROOT=${COCOS_X_ROOT}/tools/cocos2d-console/bin
-export COCOS_TEMPLATES_ROOT=${COCOS_X_ROOT}/templates
-export PATH=${PATH}:${COCOS_X_ROOT}
-export PATH=${PATH}:${COCOS_CONSOLE_ROOT}
-export PATH=${PATH}:${COCOS_TEMPLATES_ROOT}
